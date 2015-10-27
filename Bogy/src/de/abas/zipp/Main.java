56ffaafbc3a98b59a362f8f6f7b060b6b22a9141
aazi <aazi@Rampage.abasag.intra>
2015-10-27 15:43:22 +0100

    New Commit

--- Bogy/src/de/abas/zipp/Main.java
+++ Bogy/src/de/abas/zipp/Main.java
@@ -1,27 +1,22 @@
 package de.abas.zipp;
-import de.abas.zipp.behavior.AvoidCollisionBehavior;
 import de.abas.zipp.behavior.ColorSensorBehavior;
 import de.abas.zipp.behavior.DriveForwardBehavior;
-import de.abas.zipp.behavior.TouchSensorBehavior;
+import de.abas.zipp.behavior.SearchingBehavior;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
 
 public class Main {
 	
-
 	public static void main(String[] args) {
 	
 		Behavior driveForwardBehavior = new DriveForwardBehavior();
-		Behavior avoidCollisionBehavior = new AvoidCollisionBehavior();
 		Behavior colorSensorBehavior = new ColorSensorBehavior();
-		Behavior touchSensorBehavior = new TouchSensorBehavior();
+		Behavior searchingBehavior = new SearchingBehavior();
 		
-		Behavior[] bArray = { driveForwardBehavior, avoidCollisionBehavior, colorSensorBehavior, touchSensorBehavior };
+		Behavior[] bArray = {driveForwardBehavior,  colorSensorBehavior, searchingBehavior };
 		Arbitrator arbitrator = new Arbitrator(bArray);
 		
-		while(true){
-			arbitrator.start();
-		}
+		arbitrator.start();
 	}
 
 }
--- Bogy/src/de/abas/zipp/behavior/SearchingBehavior.java
+++ Bogy/src/de/abas/zipp/behavior/SearchingBehavior.java
@@ -5,21 +5,25 @@ import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.SensorMode;
 import lejos.robotics.subsumption.Behavior;
+import lejos.utility.Delay;
 
-public class ColorSensorBehavior implements Behavior {
+public class SearchingBehavior implements Behavior {
+
+	int motorDelay = 500;
 	
 	public EV3ColorSensor colorSensor;
 	
-	public ColorSensorBehavior(){
-		colorSensor = new EV3ColorSensor(SensorPort.S2);
+	public SearchingBehavior(){
+		colorSensor = new EV3ColorSensor(SensorPort.S2);	
 	}
+	
 	@Override
 	public boolean takeControl() {
+		
 		
 		SensorMode colorMode = colorSensor.getColorIDMode();
 		float sample[] = new float[colorSensor.sampleSize()];
 		colorMode.fetchSample(sample, 0);
-		System.out.println(sample[0]);
 		
 		if(sample[0] == 0.0){
 			return false;
@@ -30,14 +34,22 @@ public class ColorSensorBehavior implements Behavior {
 
 	@Override
 	public void action() {
-			
+		
+		Motor.B.stop();
+		Motor.C.stop();
+		Motor.C.rotate(540);
+		Motor.B.stop();
+		Motor.B.forward();
+		Motor.C.forward();
+		Delay.msDelay(motorDelay);
+		motorDelay += 500;
 	}
 
 	@Override
 	public void suppress() {
-	
-		Motor.B.forward();
-		Motor.C.forward();		
+		Motor.B.stop();
+		Motor.C.stop();
+
 	}
 
 }
